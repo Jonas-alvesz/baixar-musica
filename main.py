f97c13f7d0335544bb2ee49c25de78b03eb90b4d
Jonas Alves <165534987+Jonas-alvesz@users.noreply.github.com>
2024-09-08 18:10:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,25 @@ app = Flask(__name__)
 
 def download_and_convert_to_mp3(url):
     try:
-        # Baixar o vídeo do YouTube
+       
         yt = YouTube(url)
         video = yt.streams.filter(only_audio=True).first()
         video_path = video.download()
 
-        # Obter o nome do arquivo sem extensão
+       
         video_filename = os.path.splitext(os.path.basename(video_path))[0]
 
-        # Converter para MP3
+      
         audio_path = f"{video_filename}.mp3"
         video_clip = AudioFileClip(video_path)
         video_clip.write_audiofile(audio_path)
 
-        # Caminho da pasta de downloads do usuário
         user_downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
-        
-        # Mover o arquivo MP3 para a pasta de downloads do usuário
+ 
         destination_path = os.path.join(user_downloads_folder, audio_path)
         shutil.move(audio_path, destination_path)
 
-        # Excluir o arquivo de vídeo temporário
+      
         os.remove(video_path)
 
         return destination_path
@@ -45,15 +43,15 @@ def home():
 def baixar():
     url = request.form.get('url')
 
-    # Chamar a função de download e conversão
+   
     audio_file_path = download_and_convert_to_mp3(url)
 
     if audio_file_path:
-        # Se a conversão for bem-sucedida, redirecionar para a página inicial com o link de download
+        
         print("Download e conversão para MP3 concluídos com sucesso!")
         return send_file(audio_file_path, as_attachment=True)
     else:
-        # Se ocorrer um erro, redirecionar de volta para a página inicial com mensagem de erro
+        
         return redirect('/', error="Ocorreu um erro durante o download/conversão.")
 
 if __name__ == '__main__':
